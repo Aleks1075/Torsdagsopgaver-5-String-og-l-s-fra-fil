7e348f99b94cc577b576d0b7103d34be2f9106f0
Aleks1075 <buha_5@hotmail.com>
2022-04-20 00:07:32 +0200

    Update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -27,7 +27,6 @@ public class Main {
         System.out.println("\n" + "The longest word is: " + printLongestWord() + "\n");
         System.out.println(printFirstHalfOfEachWord() + "\n");
         mostRepeatedWord();
-        leastRepeatedWord();
     }
 
     //skriv dine metoder herunder:
@@ -64,30 +63,28 @@ public class Main {
         int count = 0, maxCount = 0;
         ArrayList<String> words = new ArrayList<String>();
 
-        //Opens file in read mode
         FileReader file = new FileReader("src/com/company/data.txt");
         BufferedReader br = new BufferedReader(file);
 
         //Reads each line
         while((line = br.readLine()) != null) {
             String string[] = line.toLowerCase().split("([,.\\s]+)");
-            //Adding all words generated in previous step into words
+
             for(String s : string){
                 words.add(s);
             }
         }
 
-        //Determine the most repeated word in a file
+
         for(int i = 0; i < words.size(); i++){
             count = 1;
-            //Count each word in the file and store it in variable count
+
             for(int j = i+1; j < words.size(); j++){
                 if(words.get(i).equals(words.get(j))){
                     count++;
                 }
             }
-            //If maxCount is less than count then store value of count in maxCount
-            //and corresponding word to variable word
+
             if(count > maxCount){
                 maxCount = count;
                 word = words.get(i);
